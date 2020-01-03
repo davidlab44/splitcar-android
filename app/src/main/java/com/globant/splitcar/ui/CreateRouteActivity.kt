26d3e6b9adb6971afa9ee631a3f412c1278db96e
david.mazo <david.mazo@globant.com>
2020-01-03 18:18:27 -0500

    Custom DialogFragment created within CreateRouteActivity

--- app/src/main/java/com/globant/splitcar/ui/CreateRouteActivity.kt
+++ app/src/main/java/com/globant/splitcar/ui/CreateRouteActivity.kt
@@ -3,23 +3,13 @@ package com.globant.splitcar.ui
 import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.transition.Slide
-import android.transition.TransitionManager
-import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.PopupWindow
-import android.widget.TextView
 import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -38,7 +28,6 @@ import kotlinx.android.synthetic.main.activity_create_route.editTextMeetingPlace
 import kotlinx.android.synthetic.main.activity_create_route.imageViewSaveRoute
 import kotlinx.android.synthetic.main.activity_create_route.roadReferenceListView
 import kotlinx.android.synthetic.main.activity_create_route.roadReferenceSearchInput
-import kotlinx.android.synthetic.main.activity_create_route.root_layout
 import kotlinx.android.synthetic.main.activity_create_route.spinnerCarSeat
 import kotlinx.android.synthetic.main.activity_create_route.textViewTimeRoute
 import kotlinx.android.synthetic.main.activity_create_route.textViewUser
@@ -90,78 +79,23 @@ class CreateRouteActivity : AppCompatActivity() {
                 ).show()
             }
         }
-
         showRoadReferencesDialog()
     }
 
     //TODO change this component wich is a non focusable dialog for androidx.fragment.app.DialogFragment
     private fun showRoadReferencesDialog() {
+
         button.setOnClickListener {
-            // Initialize a new layout inflater instance
-            val inflater: LayoutInflater =
-                    getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
-            // Inflate a custom view using layout inflater
-            val view = inflater.inflate(R.layout.another_view, null)
-
-            // Initialize a new instance of popup window
-            val popupWindow = PopupWindow(
-                    view, // Custom view to show in popup window
-                    LinearLayout.LayoutParams.WRAP_CONTENT, // Width of popup window
-                    LinearLayout.LayoutParams.WRAP_CONTENT // Window height
-            )
-
-            // Set an elevation for the popup window
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                popupWindow.elevation = 10.0F
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            val prev = supportFragmentManager.findFragmentByTag("dialog")
+            if (prev != null) {
+                fragmentTransaction.remove(prev)
             }
-
-            // If API level 23 or higher then execute the code
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                // Create a new slide animation for popup window enter transition
-                val slideIn = Slide()
-                slideIn.slideEdge = Gravity.TOP
-                popupWindow.enterTransition = slideIn
-
-                // Slide animation for popup window exit transition
-                val slideOut = Slide()
-                slideOut.slideEdge = Gravity.RIGHT
-                popupWindow.exitTransition = slideOut
-
-            }
-
-            // Get the widgets reference from custom view
-            val tv = view.findViewById<TextView>(R.id.text_view)
-            val buttonPopup = view.findViewById<Button>(R.id.button_popup)
-
-            // Set click listener for popup window's text view
-            tv.setOnClickListener {
-                // Change the text color of popup window's text view
-                tv.setTextColor(Color.RED)
-            }
-
-            // Set a click listener for popup's button widget
-            buttonPopup.setOnClickListener {
-                // Dismiss the popup window
-                popupWindow.dismiss()
-            }
-
-            // Set a dismiss listener for popup window
-            popupWindow.setOnDismissListener {
-                Toast.makeText(applicationContext, "Popup closed", Toast.LENGTH_SHORT).show()
-            }
-
-            // Finally, show the popup window on app
-            TransitionManager.beginDelayedTransition(root_layout)
-            popupWindow.showAtLocation(
-                    root_layout, // Location to display popup window
-                    Gravity.CENTER, // Exact position of layout to display popup
-                    0, // X offset
-                    0 // Y offset
-            )
+            fragmentTransaction.addToBackStack(null)
+            val dialogFragment = MyDialog() //here MyDialog is my custom dialog
+            dialogFragment.show(fragmentTransaction, "dialog")
         }
     }
-
 
     private fun bindComponents(email: String) {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -231,4 +165,9 @@ class CreateRouteActivity : AppCompatActivity() {
             return Intent(context, CreateRouteActivity::class.java)
         }
     }
+
 }
+
+
+
+
--- app/src/main/java/com/globant/splitcar/utils/Constants.kt
+++ app/src/main/java/com/globant/splitcar/utils/Constants.kt
@@ -13,6 +13,31 @@ const val EMAIL = "email"
 const val ROUTE_OBJECT = "Route"
 const val ROUTE_ORIGIN = "Vizcaya"
 val CARSEAT = arrayOf(1, 2, 3, 4)
+val PLACES = arrayOf(
+        "1er Parque de Laureles",
+        "Vizcaya",
+        "2do Parque de Laureles",
+        "Aldea Pablo VI",
+        "Alejandro Echavarría",
+        "Alejandría",
+        "Alfonso López",
+        "Altamira",
+        "Altavista Sector Central",
+        "Altavista",
+        "Altos del Poblado",
+        "Andalucía",
+        "Antonio Nariño",
+        "Aranjuez",
+        "Asomadera No.1",
+        "Asomadera No.2",
+        "Asomadera No.3",
+        "Astorga",
+        "Atrio Iglesia Madre de Dios",
+        "Atrio Iglesia San Bernardo",
+        "Atrio Iglesia San Gregorio Magno",
+        "Atrio Iglesia San Judas Tadeo",
+        "Atrio Parroquia Santa Maria de Jesús")
+/*
 var PLACES = arrayOf(
         "1er Parque de Laureles",
         "Vizcaya",
@@ -712,3 +737,5 @@ var PLACES = arrayOf(
         "Área de Recreación Parque Ecológico Cerro Nutibara",
         "Área de Recreación Urbana Cerro La Asomadera"
 )
+
+*/
